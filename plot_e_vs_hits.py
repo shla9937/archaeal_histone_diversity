deda081f3b6690669ceaf639a453df9f27d23d60
shla9937 <shla9937@colorado.edu>
2024-08-29 10:45:13 -0600

    update

---
+++ plot_e_vs_hits.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import argparse
+from decimal import Decimal, getcontext
+
+def main():
+    parser = argparse.ArgumentParser(description='Plot data from multiple files.')
+    parser.add_argument('--files', nargs='+', type=str, required=True, help='List of input files')
+    parser.add_argument('--names', nargs='+', type=str, required=True, help='List of names corresponding to each file')
+    args = parser.parse_args()
+
+    if len(args.files) != len(args.names):
+        raise ValueError("The number of files and names must be the same.")
+
+    global cud_palette
+    cud_palette = ["#56B4E9","#D55E00","#E69F00","#F0E442","#009E73","#CC79A7","#0072B2","#000000", "#999999"]
+
+    getcontext().prec = 100
+
+    plt.figure(figsize=(10, 6))
+
+    for i, file in enumerate(args.files):
+        data = pd.read_csv(file, sep="\t", header=None, dtype=str)
+        data[0] = data[0].apply(Decimal)
+        data[1] = data[1].apply(Decimal)
+        plt.plot(data[0], data[1], label=args.names[i], color=cud_palette[i % len(cud_palette)])
+
+
+    plt.xscale('log')
+    plt.xlim(1e-40, 1e10)
+    plt.xlabel("E-value")
+    plt.ylabel("Hits")
+    plt.title("Hits at a give E-value")
+    plt.legend(loc='best')
+    plt.tight_layout()
+    
+    plt.savefig("../outputs/e_value_comparison.png")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
--- plot_venn.py
+++ plot_venn.py
@@ -6,25 +6,21 @@ import matplotlib.pyplot as plt
 from matplotlib_venn import venn2, venn3
 
 def main():
-    # Set up argparse to take in multiple CSV files
     parser = argparse.ArgumentParser(description='Create a Venn diagram from protein CSV files.')
     parser.add_argument('--csvs', type=str, required=True, nargs='+', help='Paths to the input CSV files')
     parser.add_argument('--names', type=str, required=True, nargs='+', help='Names of datasets')
     parser.add_argument('--col', type=str, required=True, nargs='+', help='Column to venn')
     args = parser.parse_args()
 
-    # Read the CSV files into DataFrames
     dataframes = [pd.read_csv(csv_file) for csv_file in args.csvs]
     names = [i for i in args.names]
 
-    # Ensure there are at least two DataFrames
+    global cud_palette
+    cud_palette = ["#56B4E9","#D55E00","#E69F00","#F0E442","#009E73","#CC79A7","#0072B2","#000000", "#999999"]
+
     if len(dataframes) < 2:
         raise ValueError('At least two CSV files are required to create a Venn diagram.')
-
-    # Extract the sets of 'accession' values
     sets = [set(df['Protein Name']) for df in dataframes]
-    
-    # Create Venn diagram based on the number of sets
     plt.figure(figsize=(8, 8))
     if len(sets) == 2:
         venn2(sets, set_labels=[f'{names[i]}' for i in range(len(sets))])
@@ -32,8 +28,6 @@ def main():
         venn3(sets, set_labels=[f'{names[i]}' for i in range(len(sets))])
     else:
         raise ValueError('This script currently supports up to 3 CSV files for Venn diagrams.')
-
-    # Set title and show plot
     plt.title('Venn Diagram of Protein Accessions')
     plt.show()
 
