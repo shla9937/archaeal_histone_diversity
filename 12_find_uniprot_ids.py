e27606accb56ff091af053c1819e5562e46f19d7
shla9937 <shla9937@colorado.edu>
2025-10-09 07:45:13 -0600

    working uniprot adder

--- 12_find_uniprot_ids.py
+++ 12_find_uniprot_ids.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import pandas as pd
 import argparse
@@ -7,36 +7,29 @@ import ast
 def main():
     parser = argparse.ArgumentParser(description="Search for UniProt IDs based on histone sequences.")
     parser.add_argument('--df', type=str, required=True, help="Path to the CSV file with protein sequences.")
+    parser.add_argument('--ids', type=str, required=True, help="Path to the TSV file with accessions.")
     args = parser.parse_args()
 
-    sequences_df = pd.read_csv(args.df)
-    uniprots = find_uniprot_ids(sequences_df)
-    full_df = add_sequences_column(sequences_df, uniprots)
+    protein_df = pd.read_csv(args.df)
+    uniprots = pd.read_csv(args.ids,sep='\t',header=None,names=['UniProtID', 'Score'],dtype=str)
+    full_df = add_sequences_column(protein_df, uniprots)
     output_strategies_df(full_df)
 
-def find_uniprot_ids(sequences_df):
-    sequences = []
-    for histone in histone_list:
-        seq_row = sequences_df[sequences_df['Protein Name'] == histone]
-        if not seq_row.empty:
-            sequences.append(seq_row['Sequence'].values[0])
-    return sequences
-    print(sequences_df['Sequences'])
-    True
-
-def add_sequences_column(strategies_df, sequences_df):
-    sequences_column = strategies_df['Histones'].apply(
-        lambda histone_list: get_sequence_for_histones(histone_list, sequences_df)
-    )
-    histones_index = strategies_df.columns.get_loc('Histones')
-    strategies_df.insert(histones_index + 1, 'Sequences', sequences_column)
-    return strategies_df
-    True
+def add_sequences_column(protein_df, uniprots):
+    protein_df.insert(4, 'UniProtIDs', None)
+    protein_df['Histones'] = protein_df['Histones'].apply(ast.literal_eval)
+    for index, row in protein_df.iterrows():
+        uniprot_list = []
+        for histone in row['Histones']:
+            for index_id, row_id in uniprots.iterrows():
+                if index_id == histone.lstrip('>'):
+                    uniprot_list.append(row_id.UniProtID)
+        protein_df.loc[index, 'UniProtIDs'] = str(uniprot_list)
+    return protein_df
 
 def output_strategies_df(strategies_df):
-    output_path = '../outputs/proteins_clustered_taxonomy_strategy_sequence_uniprot_db.csv'
+    output_path = '../proteins_clustered_taxonomy_strategy_sequence_uniprot_db.csv'
     strategies_df.to_csv(output_path, index=False)
-    print(f"Updated strategies_df saved to {output_path}")
-    True
+
 if __name__ == "__main__":
     main()
